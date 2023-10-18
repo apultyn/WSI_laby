f15e469fdbd2fdba9a3378a460027bbd9fd4bb2d
Andrzej Pultyn <a.pultyn@stud.pw.edu.pl>
2023-10-18 10:22:30 +0200

    Seperated plotting from main

--- lab1/main.py
+++ lab1/main.py
@@ -20,49 +20,55 @@ def gradient_g(x):
     return np.array([2 * x[0] * np.exp(-x[0]**2 - x[1]**2) + (x[0] - 1) *
                      np.exp(-((x[0] - 1)**2) - ((x[1] + 2)**2)),
                      2 * x[1] * np.exp(-x[0]**2 - x[1]**2) + (x[1] + 2) *
-                     np.exp(-((x[0] - 1)**2) - ((x[1] + 2)**2))
-                     ])
+                     np.exp(-((x[0] - 1)**2) - ((x[1] + 2)**2))])
+
+
+def plot_2D(function, trajectory, x_values, title):
+    y_values = function(x_values)
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(x_values, y_values, label='f(x)')
+    ax.scatter(trajectory, function(trajectory), c='red', label='Trajectory')
+    ax.set_title(title)
+    ax.set_xlabel('x')
+    ax.set_ylabel('f(x)')
+    ax.legend()
+
+    plt.show()
+
+
+def plot_3D(function, trajectory, x_values, title):
+    y_values = np.zeros((len(x_values), len(x_values)))
+
+    for i in range(len(x_values)):
+        for j in range(len(x_values)):
+            y_values[i, j] = function(np.array([x_values[i], x_values[j]]))
+
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    X, Y = np.meshgrid(x_values, x_values)
+    Z = y_values
+    ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+    ax.scatter(trajectory[:, 0], trajectory[:, 1], function(trajectory.T),
+               c='red', label='Trajectory')
+    ax.set_title(title)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.legend()
+
+    plt.show()
 
 
 def main():
     func_f = Gradient_descent(f, gradient_f, 1, 0.1, 0.001)
     trajectory_f = func_f.solve(np.array([-2.5]))
+    x_values_f = np.linspace(-4, 4, 100)
+    plot_2D(f, trajectory_f, x_values_f, 'Gradient Descent for f(x)')
 
     func_g = Gradient_descent(g, gradient_g, 0.5, 0.1, 0.001)
     trajectory_g = func_g.solve(np.array([-1.5, 2]))
-
-    x_values_f = np.linspace(-4, 4, 100)
     x_values_g = np.linspace(-4, 4, 100)
-    y_values_f = f(x_values_f)
-    y_values_g = np.zeros((100, 100))
-
-    for i in range(100):
-        for j in range(100):
-            y_values_g[i, j] = g(np.array([x_values_g[i], x_values_g[j]]))
-
-    fig = plt.figure(figsize=(12, 6))
-
-    ax1 = fig.add_subplot(121)
-    ax1.plot(x_values_f, y_values_f, label='f(x)')
-    ax1.scatter(trajectory_f, f(trajectory_f), c='red', label='Trajectory')
-    ax1.set_title('Gradient Descent for f(x)')
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('f(x)')
-    ax1.legend()
-
-    ax2 = fig.add_subplot(122, projection='3d')
-    X, Y = np.meshgrid(x_values_g, x_values_g)
-    Z = y_values_g
-    ax2.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
-    ax2.scatter(trajectory_g[:, 0], trajectory_g[:, 1],
-                g(trajectory_g.T), c='red', label='Trajectory')
-    ax2.set_title('Gradient Descent for g(x)')
-    ax2.set_xlabel('x1')
-    ax2.set_ylabel('x2')
-    ax2.legend()
-
-    plt.savefig("answer.pdf")
-    plt.show()
+    plot_3D(g, trajectory_g, x_values_g, 'Gradient Descent for g(x)')
 
 
 if __name__ == "__main__":
