c15647523624ea834be1fc264bbeb454db43d455
Andrzej Pultyn <a.pultyn@stud.pw.edu.pl>
2023-11-06 15:11:45 +0100

    Correctly calcing route legth and sorting it

--- lab2/main.py
+++ lab2/main.py
@@ -2,8 +2,9 @@ from tsp import TSP
 
 
 def main():
-    solver = TSP([[-20, -20], [-24, -20]])
-    solver.solve(100, 30, 8, 4, 4)
+    solver = TSP([[1, 1], [2, 0], [-1, -1], [-2, 0], [0, 0], [-1, 1], [1, -1]])
+    solver.solve(100, 10, 4, 2, 2)
+
 
 if __name__ == "__main__":
     main()
--- lab2/tsp.py
+++ lab2/tsp.py
@@ -8,8 +8,7 @@ class TSP(Solver):
         self._epochs = None
         self._starting_population = None
         self._parents = None
-        self._offsprings = None
-        self._dying_amount = None
+        self._mutate_rate = None
 
     def get_parameters(self):
         """Returns last sollution hyperparameters"""
@@ -18,17 +17,20 @@ class TSP(Solver):
             "epochs": self._epochs,
             "starting_population": self._starting_population,
             "parents": self._parents,
-            "offsprings": self._offsprings,
-            "dying_amount": self._dying_amount
+            "mutate_rate": self._mutate_rate
         }
 
     def solve(self, epochs, starting_population,
-              parents, offsprings, dying_amount):
+              parents, mutate_rate):
         self._epochs = epochs
         self._starting_population = starting_population
         self._parents = parents
-        self._offsprings = offsprings
-        self._dying_amount = dying_amount
+        self._mutate_rate = mutate_rate
+
+        if parents > starting_population:
+            raise ValueError("Not enough population for this amount of parents")
+        if parents % 2 != 0:
+            raise ValueError("Amount of parents must be even number")
 
         population = []
         for _ in range(starting_population):
@@ -36,10 +38,19 @@ class TSP(Solver):
             value = 0
             for i in range(len(order) - 1):
                 value += np.sqrt(
-                    np.square(self._cities[order[i+1]][0] - self._cities[order[i]][0]) +
-                    np.square(self._cities[order[i+1]][1] - self._cities[order[i]][1]))
+                    np.square(self._cities[order[i+1]][0] -
+                              self._cities[order[i]][0]) +
+                    np.square(self._cities[order[i+1]][1] -
+                              self._cities[order[i]][1]))
             value += np.sqrt(
-                np.square(self._cities[order[-1]][0] - self._cities[order[0]][0]) +
-                np.square(self._cities[order[-1]][1] - self._cities[order[0]][1]))
+                np.square(self._cities[order[-1]][0] -
+                          self._cities[order[0]][0]) +
+                np.square(self._cities[order[-1]][1] -
+                          self._cities[order[0]][1]))
             population.append((order, value))
+
+        def sort_val(x):
+            return x[1]
+        population.sort(reverse=True, key=sort_val)
+
         print(population)
