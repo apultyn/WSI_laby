1df4a1a05c86f40685e35a9f5070d833daf97a7d
Andrzej Pultyn <a.pultyn@stud.pw.edu.pl>
2023-11-06 14:42:07 +0100

    Calculating value of one individual

--- lab2/main.py
+++ lab2/main.py
@@ -2,9 +2,8 @@ from tsp import TSP
 
 
 def main():
-    solver = TSP()
-    solver.solve()
-
+    solver = TSP([[-20, -20], [-24, -20]])
+    solver.solve(100, 30, 8, 4, 4)
 
 if __name__ == "__main__":
     main()
--- lab2/tsp.py
+++ lab2/tsp.py
@@ -1,5 +1,45 @@
 from solver import Solver
+import numpy as np
 
 
 class TSP(Solver):
-    pass
+    def __init__(self, cities: list):
+        self._cities = cities
+        self._epochs = None
+        self._starting_population = None
+        self._parents = None
+        self._offsprings = None
+        self._dying_amount = None
+
+    def get_parameters(self):
+        """Returns last sollution hyperparameters"""
+        return {
+            "cities": self._cities,
+            "epochs": self._epochs,
+            "starting_population": self._starting_population,
+            "parents": self._parents,
+            "offsprings": self._offsprings,
+            "dying_amount": self._dying_amount
+        }
+
+    def solve(self, epochs, starting_population,
+              parents, offsprings, dying_amount):
+        self._epochs = epochs
+        self._starting_population = starting_population
+        self._parents = parents
+        self._offsprings = offsprings
+        self._dying_amount = dying_amount
+
+        population = []
+        for _ in range(starting_population):
+            order = np.random.permutation(len(self._cities))
+            value = 0
+            for i in range(len(order) - 1):
+                value += np.sqrt(
+                    np.square(self._cities[order[i+1]][0] - self._cities[order[i]][0]) +
+                    np.square(self._cities[order[i+1]][1] - self._cities[order[i]][1]))
+            value += np.sqrt(
+                np.square(self._cities[order[-1]][0] - self._cities[order[0]][0]) +
+                np.square(self._cities[order[-1]][1] - self._cities[order[0]][1]))
+            population.append((order, value))
+        print(population)
