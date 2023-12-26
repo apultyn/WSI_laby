03f12402c57a5a1d2535339b7aac5220a5cb6a24
Andrzej Pultyn <a.pultyn@stud.pw.edu.pl>
2023-12-26 23:07:34 +0100

    Working better

--- lab5/svm.py
+++ lab5/svm.py
@@ -6,79 +6,71 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 class SVM:
-    def __init__(self, epochs=50, learning_rate=0.001, lambda_param=0.01):
-        self.epochs = epochs
+    def __init__(self, learning_rate, lambda_param):
         self.learning_rate = learning_rate
         self.lambda_param = lambda_param
         self.weights = None
-        self.bias = None
-
-    def train(self, X, y, class_label):
-        n_samples, n_features = X.shape
-        y_ = np.where(y <= 0, -1, 1)
-
-        self.weights = np.zeros(n_features)
         self.bias = 0
 
-        for epoch in range(self.epochs):
-            for image in range(n_samples):
-                condition = y_[image] * (np.dot(X[image], self.weights) - self.bias) >= 1
-                if condition:
-                    self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights)
-                else:
-                    self.weights -= self.learning_rate * (
-                        2 * self.lambda_param * self.weights - np.dot(X[image], y_[image])
-                    )
-                    self.bias -= self.learning_rate * y_[image]
-                print(f"Label {class_label}, epoch {epoch}, image {image}")
+    def train(self, train_images, train_labels):
+        n_samples, n_features = train_images.shape
+        y_ = np.where(train_labels <= 0, -1, 1)
+
+        if self.weights is None:
+            self.weights = np.zeros(n_features)
+
+        for image in range(n_samples):
+            condition = y_[image] * (np.dot(train_images[image], self.weights) - self.bias) >= 1
+            if condition:
+                self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights)
+            else:
+                self.weights -= self.learning_rate * (
+                    2 * self.lambda_param * self.weights - np.dot(train_images[image], y_[image])
+                )
+                self.bias -= self.learning_rate * y_[image]
 
     def predict(self, X):
         linear_output = np.dot(X, self.weights) - self.bias
         return np.sign(linear_output)
 
-    def evaluate(self, X, y):
-        y_pred = self.predict(X)
-        accuracy = np.mean(y_pred == y)
-        print("Accuracy:", accuracy)
-
 
 class MulticlassSVM:
-    def __init__(self, epochs=100, learning_rate=0.001, lambda_param=0.01):
-        self.epochs = epochs
-        self.learning_rate = learning_rate
-        self.lambda_param = lambda_param
+    def __init__(self):
         self.classifiers = {}  # Dictionary to store binary classifiers
 
-    def train(self, X, y):
-        unique_classes = np.unique(y)
+    def train(self, train_images, train_labels,
+              test_images, test_labels,
+              epochs, learning_rate=0.001, lambda_param=0.01):
+        unique_classes = np.unique(train_labels)
+        svm = None
+        for i in range(epochs):
+            for class_label in unique_classes:
+                binary_labels = np.where(train_labels == class_label, 1, -1)
+                if class_label in self.classifiers:
+                    svm = self.classifiers[class_label]
+                else:
+                    svm = SVM(learning_rate, lambda_param)
+                svm.train(train_images, binary_labels)
+                self.classifiers[class_label] = svm
+            print(f"Accuracy for epoch {i}: {self.evaluate(test_images, test_labels)}")
 
-        for class_label in unique_classes:
-            # Convert to binary classification (class vs. rest)
-            binary_labels = np.where(y == class_label, 1, -1)
-
-            # Train a binary classifier for the current class
-            svm = SVM(self.epochs, self.learning_rate, self.lambda_param)
-            svm.train(X, binary_labels, class_label)
-            self.classifiers[class_label] = svm
-
-    def predict(self, X):
+    def predict(self, image):
         class_scores = {}
 
         for class_label, svm in self.classifiers.items():
-            class_scores[class_label] = svm.predict(X)
+            class_scores[class_label] = svm.predict(image)
 
-        # Choose the class with the highest decision function output
         predicted_class = max(class_scores, key=lambda k: class_scores[k])
 
         return predicted_class
 
-    def evaluate(self, X, y):
-        y_pred = np.array([self.predict(sample) for sample in X])
-        accuracy = accuracy_score(y, y_pred)
-        print("Accuracy:", accuracy)
+    def evaluate(self, test_images, test_labels):
+        y_pred = np.array([self.predict(sample) for sample in test_images])
+        return accuracy_score(test_labels, y_pred)
 
-        # Create and print the confusion matrix with labels
-        confusion_mat = confusion_matrix(y, y_pred, labels=np.unique(y))
+    def confusion_matrix(self, test_images, test_labels):
+        y_pred = np.array([self.predict(sample) for sample in test_images])
+        confusion_mat = confusion_matrix(test_labels, y_pred, labels=np.unique(test_labels))
         print("Confusion Matrix:")
         print(confusion_mat)
 
@@ -102,16 +94,10 @@ def main(arguments):
     train_images = train_images.reshape(len(train_images), -1) / 255
     test_images = test_images.reshape(len(test_images), -1) / 255
 
-    # train_images = train_images[:1000]
-    # train_labels = train_labels[:1000]
-    # test_images = test_images[:100]
-    # test_labels = test_labels[:100]
-
-
-
-    multiclass_svm = MulticlassSVM(epochs, lambda_param, learning_rate)
-    multiclass_svm.train(train_images, train_labels)
-    multiclass_svm.evaluate(test_images, test_labels)
+    multiclass_svm = MulticlassSVM()
+    multiclass_svm.train(train_images, train_labels, test_images,
+                         test_labels, epochs, learning_rate, lambda_param)
+    multiclass_svm.confusion_matrix(test_images, test_labels)
 
     unique_labels, label_counts = np.unique(test_labels, return_counts=True)
     print("Tests:")
